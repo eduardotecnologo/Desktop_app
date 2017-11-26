abceb8e450616c773a28758a89f633873a6f28d9
eduardotecnologo <eduardotecnologo@hotmail.com>
2017-11-26 11:11:55 -0200

    Projeto Base64 - Lógica 01

--- electron-quick-start/scripts/app.js
+++ electron-quick-start/scripts/app.js
@@ -1,3 +1,19 @@
-function onClick(){
-    alert('Opa!!!')
+// function onClick(){
+//     alert('Opa!!!')
+// }
+function generate(){
+    var inputTxt = document.getElementById('txt');
+    if(!inputTxt.value){
+        alert('Please input something');
+        return;
+    }
+    var base64 = btoa(inputTxt.value);
+    var span = document.createElement('span');
+
+    span.innerHTML = base64;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(span);
+}
+function decode(){
+
 }
